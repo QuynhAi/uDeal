1f9223143b076c72ed1959141539219a1d69329f
ainguyen <40418101+QuynhAi@users.noreply.github.com>
2020-04-15 15:55:31 -0700

    added bottom toolbar

---
+++ app/src/main/java/edu/tacoma/uw/udeal/Camera.java
@@ -0,0 +1,19 @@
+package edu.tacoma.uw.udeal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class Camera extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_camera, container, false);
+    }
+}
