944a57e93eb23fd9ae2d4b1c98e10e955685ce92
dungthai85 <Dungthai85@gmail.com>
2020-04-20 17:05:46 -0700

    inbox

--- app/src/main/java/edu/tacoma/uw/udeal/Chat.java
+++ app/src/main/java/edu/tacoma/uw/udeal/Chat.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 
@@ -15,6 +16,13 @@ public class Chat extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Inbox");
+//        //To enable home icon on toolbar
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        //To change the icon
+////        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button);
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setLogo(R.drawable.back_button);
+
+
         return inflater.inflate(R.layout.activity_inbox_screen, container, false);
     }
 }
