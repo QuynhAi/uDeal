4f3d6a78835d3e8cfcf649fcb1c0beb15856be7c
ainguyen <40418101+QuynhAi@users.noreply.github.com>
2020-04-22 19:31:47 -0700

    login with correct credentials

--- app/src/main/java/edu/tacoma/uw/udeal/Login.java
+++ app/src/main/java/edu/tacoma/uw/udeal/Login.java
@@ -3,18 +3,35 @@ package edu.tacoma.uw.udeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import model.User;
+import model.UserLogin;
+
 public class Login extends AppCompatActivity {
 
     private EditText emailEdit;
     private EditText passwordEdit;
-
+    private JSONObject mArguments;
+    private String TAG = "Login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +46,9 @@ public class Login extends AppCompatActivity {
             public void onClick(View v){
                 String email = emailEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
-                if (email.isEmpty() || password.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "enter email or password" , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+                UserLogin userLogin = new UserLogin(email, password);
+                loginMethod(userLogin);
+
             }
         });
 
@@ -47,5 +61,70 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    public void loginMethod(UserLogin userLogin) {
+        StringBuilder url = new StringBuilder(getString(R.string.login));
+        //Construct a JSONObject to build a formatted message to send.
+        mArguments = new JSONObject();
+        try {
+            mArguments.put(UserLogin.EMAIL, userLogin.getEmail());
+            mArguments.put(UserLogin.PASSWORD, userLogin.getPassword());
+            new LoginAsyncTask().execute(url.toString());
+        } catch (JSONException e) {
+            Toast.makeText(getApplicationContext(), "Error with JSON creation: " + e.getMessage() , Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    private class LoginAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.setDoOutput(true);
+                    OutputStreamWriter wr =
+                            new OutputStreamWriter(urlConnection.getOutputStream());
+                    Log.i(TAG, mArguments.toString());
+                    wr.write(mArguments.toString());
+                    wr.flush();
+                    wr.close();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception e) {
+                    response = e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            try{
+                JSONObject resultObject = new JSONObject(result);
+                if(resultObject.getBoolean("success") == true){
+                    Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Credentials did not match or missing information ", Toast.LENGTH_SHORT).show();
+                    //Log.e(TAG, resultObject.getString("error"));
+                }
+            }catch(JSONException e){
+                Toast.makeText(getApplicationContext(), e.getMessage() , Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
 
 }
--- app/src/main/java/model/UserLogin.java
+++ app/src/main/java/model/UserLogin.java
@@ -10,16 +10,13 @@ public class UserLogin {
 
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
-    public static final String UNIQUE_ID = "uniqueID";
 
     public UserLogin( String email, String password) {
 
         this.email = email;
         this.password = password;
-        this.uniqueId = uniqueId;
     }
 
     public String getEmail(){return email;}
     public String getPassword(){return password;}
-    public String getUniqueId() { return uniqueId; }
 }
