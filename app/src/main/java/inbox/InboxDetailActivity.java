5be30e334b60497163eedc26de0ded026c129842
dungthai85 <Dungthai85@gmail.com>
2020-04-28 12:51:50 -0700

    fixed chat bar size

--- app/src/main/java/inbox/InboxDetailActivity.java
+++ app/src/main/java/inbox/InboxDetailActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
