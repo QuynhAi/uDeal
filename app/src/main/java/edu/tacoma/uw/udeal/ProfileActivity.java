ff08251133fb64fe22df1930280a9af2d80e5bba
mzloria <mikeeloria@gmail.com>
2020-05-07 13:46:38 -0700

    Added Logout screen on user profile

--- app/src/main/java/edu/tacoma/uw/udeal/ProfileActivity.java
+++ app/src/main/java/edu/tacoma/uw/udeal/ProfileActivity.java
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +39,27 @@ public class ProfileActivity extends AppCompatActivity {
 //        Fragment personfragment = new Person();
 //        fragmentTransaction.add(R.id.fragment_container, personfragment);
 //        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            SharedPreferences sharedPreferences =
+                    getSharedPreferences(getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
+            sharedPreferences.edit().putBoolean(getString(R.string.LOGGEDIN), false)
+                    .commit();
+
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
