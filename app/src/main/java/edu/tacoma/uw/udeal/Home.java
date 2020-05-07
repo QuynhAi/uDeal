702ad8e4c3612ed95d0ab960b3e9e69c2c92f56e
mzloria <mikeeloria@gmail.com>
2020-05-07 12:46:10 -0700

    Testing ability to display image from S3

--- app/src/main/java/edu/tacoma/uw/udeal/Home.java
+++ app/src/main/java/edu/tacoma/uw/udeal/Home.java
@@ -4,18 +4,124 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Home extends Fragment {
 
+    private ImageView myImageView;
+    private JSONArray values;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Home");
-        return inflater.inflate(R.layout.activity_home, container, false);
+        View view = inflater.inflate(R.layout.activity_home, container, false);
+        myImageView = (ImageView) view.findViewById(R.id.displayphoto);
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        StringBuilder urlTextFields = new StringBuilder(getString(R.string.download));
+        urlTextFields.append("?myfilename=f93fda4f-5cfc-4faf-8744-c2fdfb8d7e74.jpg");
+        super.onResume();
+        //if (myImageView == null) {
+          //  Log.d("myTag", "imageview is null");
+        //    new ImageTask().execute(urlTextFields.toString());
+      //  }
+    }
+
+    private class ImageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to download the image, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.startsWith("Unable to")) {
+                Toast.makeText(getActivity().getApplicationContext(), "Unable to download" + s, Toast.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+            try {
+                Log.d("myTag","WE SUCCESSFULLY ESTABLISHED URL CONNECTION");
+                JSONObject jsonObject = new JSONObject(s);
+                Log.d("myTag", jsonObject.toString());
+                if (jsonObject.getBoolean("success")) {
+                    Log.d("myTag","We are inside the success feature");
+                    values = jsonObject.getJSONObject("values").getJSONObject("Body").getJSONArray("data");
+                    Bitmap bitmap = null;
+                    byte[] tmp= new byte[values.length()];
+                    for(int i=0;i< values.length(); i++){
+                        tmp[i]=(byte)(((int) values.get(i)) & 0xFF);
+                    }
+                    bitmap = BitmapFactory.decodeByteArray(tmp, 0, tmp.length);
+                    myImageView.setImageBitmap(bitmap);
+              /*      Log.d("myTag","We have gotten the values");
+                    Log.d("myTag",values.toString());
+                    byte[] data = values.toString().getBytes("UTF-8");
+                    Log.d("myTag", "WE HAVE SUCCESSFULLY GOTTEN THE BITES");
+                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    if(bmp == null) {
+                        Log.d("myTag", "BMP IS NULL");
+                    }
+                    myImageView.setImageBitmap(bmp);
+                    Log.d("myTag", "IMAGE IS SET"); */
+                //    if (!mCourseList.isEmpty()) {
+                //        setupRecyclerView((RecyclerView) mRecyclerView);
+               //     }
+                }
+
+            } catch (JSONException e) {
+                Log.d("myTag","FAILURE");
+                Toast.makeText(getActivity().getApplicationContext(), "JSON Error: " + e.getMessage(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
