9d87b64728294c7bd53e137edbbd267b43b44df9
mzloria <mikeeloria@gmail.com>
2020-05-15 12:48:35 -0700

    Javadoc for splash screen

--- app/src/main/java/edu/tacoma/uw/udeal/SplashScreen.java
+++ app/src/main/java/edu/tacoma/uw/udeal/SplashScreen.java
@@ -6,9 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-
+/**
+ * The splash screen for the application.
+ *
+ * @author TCSS 450 Team 8
+ * @version 1.0
+ */
 public class SplashScreen extends AppCompatActivity {
+
+    /** The handler for the splash screen. */
     Handler handler;
+
+    /**
+     * Creates the splash screen then starts the login activity.
+     *
+     * @param savedInstanceState The saved instance state. 
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +31,6 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 Intent intent=new Intent(SplashScreen.this, Login.class);
                 startActivity(intent);
                 finish();
