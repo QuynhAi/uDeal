de0138ffc95b437aed4df146bee34905d6bffbb2
mzloria <mikeeloria@gmail.com>
2020-05-15 13:44:30 -0700

    Javadoc for userlogin

--- app/src/main/java/model/UserLogin.java
+++ app/src/main/java/model/UserLogin.java
@@ -1,22 +1,52 @@
 package model;
 
+/**
+ * This class handles the user login information.
+ *
+ * @author TCSS 450 Team 8
+ * @version 1.0
+ */
 public class UserLogin {
 
-
+    /** The email of the user. */
     private String email;
+
+    /** The password of the user. */
     private String password;
-    private String uniqueId;
 
-
+    /** Email string. */
     public static final String EMAIL = "email";
+
+    /** Password string. */
     public static final String PASSWORD = "password";
 
+    /**
+     * Initializes the email and the password.
+     *
+     * @param email The email
+     * @param password The password
+     */
     public UserLogin( String email, String password) {
 
         this.email = email;
         this.password = password;
     }
 
-    public String getEmail(){return email;}
-    public String getPassword(){return password;}
+    /**
+     * Gets the email.
+     *
+     * @return The email.
+     */
+    public String getEmail(){
+        return email;
+    }
+
+    /**
+     * Gets the password.
+     *
+     * @return The password.
+     */
+    public String getPassword(){
+        return password;
+    }
 }
