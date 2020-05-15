0c7d5830b1ffc1bc00388f916e121caf25312c48
mzloria <mikeeloria@gmail.com>
2020-05-15 11:48:34 -0700

    Javadoc for item class

--- app/src/main/java/model/Item.java
+++ app/src/main/java/model/Item.java
@@ -1,33 +1,58 @@
 package model;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * This class handles the item used when posting
+ * the item to the database.
+ *
+ * @author TCSS 450 Team 8
+ * @version 1.0
+ */
 public class Item implements Serializable {
 
+    /** Used to retrieve item ID. */
+    private static final String ITEM_ID = "item_id";
+    /** Used to retrieve member ID. */
+    private static final String MEMBER_ID = "member_id";
+    /** Used to retrieve the title. */
+    private static final String TITLE = "title";
+    /** Used to retrieve the location. */
+    private static final String LOCATION = "location";
+    /** Used to retrieve the description. */
+    private static final String DESCRIPTION = "description";
+    /** Used to retrieve the category. */
+    private static final String CATEGORY = "category";
+    /** Used to retrieve the price. */
+    private static final String PRICE = "price";
+
+    /** The item ID. */
     private int mItemID;
+    /** The member ID. */
     private int mMemberID;
+    /** The title. */
     private String mTitle;
+    /** The location of the item. */
     private String mLocation;
+    /** The description of the item. */
     private String mDescription;
+    /** The category of the item. */
     private String mCategory;
+    /** The price of the item. */
     private double mPrice;
 
-    public static final String ITEM_ID = "item_id";
-    public static final String MEMBER_ID = "member_id";
-    public static final String TITLE = "title";
-    public static final String LOCATION = "location";
-    public static final String DESCRIPTION = "description";
-    public static final String CATEGORY = "category";
-    public static final String PRICE = "price";
 
+    /**
+     * Initializes the fields of the itmem.
+     *
+     * @param theMemberID The member ID
+     * @param theTitle The title
+     * @param theLocation The location
+     * @param theDescription The description
+     * @param theCategory The category
+     * @param thePrice The price
+     */
     public Item(int theMemberID, String theTitle, String theLocation, String theDescription, String theCategory, double thePrice) {
-  //      mItemID = theItemID;
         mMemberID = theMemberID;
         mTitle = theTitle;
         mLocation = theLocation;
@@ -36,57 +61,57 @@ public class Item implements Serializable {
         mPrice = thePrice;
     }
 
-    public int getmItemID() {return mItemID;}
-
-    public int getmMemberID() {return mMemberID;}
-
-    public String getmTitle() {return mTitle;}
-
-    public String getmLocation() {return mLocation;}
-
-    public String getmDescription() {return mDescription;}
-
-    public String getmCategory() {return mCategory;}
-
-    public double getmPrice() {return mPrice;}
-
-    public void setmItemID(int theItemID) {mItemID = theItemID;}
-
-    public void setmMemberID(int theMemberID) {mMemberID = theMemberID;}
-
-    public void setmTitle(String theTitle) {mTitle = theTitle;}
-
-    public void setmLocation(String theLocation) {mLocation = theLocation;}
-
-    public void setmDescription(String theDescription) {mDescription = theDescription;}
-
-    public void setmCategory(String theCategory) {mCategory = theCategory;}
-
-    public void setmPrice(double thePrice) {mPrice = thePrice;}
-
-    public static List<Item> parsesItemJson(String itemJson) throws JSONException {
-        List<Item> itemList = new ArrayList<>();
-        if (itemJson != null) {
-
-            JSONArray arr = new JSONArray(itemJson);
-
-            for (int i = 0; i < arr.length(); i++) {
-                JSONObject obj = arr.getJSONObject(i);
-                Item userItem = new Item(obj.getInt(Item.MEMBER_ID),
-                        obj.getString(Item.TITLE),
-                        obj.getString(Item.LOCATION),
-                        obj.getString(Item.DESCRIPTION),
-                        obj.getString(Item.CATEGORY),
-                        obj.getDouble(Item.PRICE));
-                userItem.setmItemID(obj.getInt(Item.ITEM_ID));
-                // we have to set item id after to account for the fact that we dont have access to
-                // the id when creating the item object
-                // MAKE SURE TO GET ITEM ID AFTER POST, AND SAVE
-                itemList.add(userItem);
-            }
-        }
-        return itemList;
+    /**
+     * Gets the member ID.
+     *
+     * @return The member ID
+     */
+    public int getmMemberID() {
+        return mMemberID;
     }
 
+    /**
+     * Gets the title.
+     *
+     * @return The title
+     */
+    public String getmTitle() {
+        return mTitle;
+    }
 
+    /**
+     * Gets the location.
+     *
+     * @return The item location
+     */
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Gets the item ID.
+     *
+     * @return The item ID
+     */
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    /**
+     * Gets the item description.
+     *
+     * @return The item description
+     */
+    public String getmCategory() {
+        return mCategory;
+    }
+
+    /**
+     * Gets the item price.
+     *
+     * @return The item price
+     */
+    public double getmPrice() {
+        return mPrice;
+    }
 }
