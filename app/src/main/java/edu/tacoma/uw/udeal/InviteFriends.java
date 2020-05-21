f0a3e7f1882927e475da67491ef1ed3ac93a0a97
mzloria <mikeeloria@gmail.com>
2020-05-20 18:06:29 -0700

    Redid invite friends

--- app/src/main/java/edu/tacoma/uw/udeal/InviteFriends.java
+++ app/src/main/java/edu/tacoma/uw/udeal/InviteFriends.java
@@ -1,9 +1,14 @@
 package edu.tacoma.uw.udeal;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,9 +33,23 @@ public class InviteFriends extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        getActivity().setTitle("Invite Friends");
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.invite_friends, container, false);
+        View view = inflater.inflate(R.layout.invite_friends, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitle("Invite Friends");
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_ATOP);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
     /**
