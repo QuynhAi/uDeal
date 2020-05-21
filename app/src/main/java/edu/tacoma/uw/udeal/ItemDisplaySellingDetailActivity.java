5b67cfeec3a0e03a8f70560e31c30e7ecc18d8a2
mzloria <mikeeloria@gmail.com>
2020-05-21 01:32:20 -0700

    Removed fab icon, and added switch to delist and list item

--- app/src/main/java/edu/tacoma/uw/udeal/ItemDisplaySellingDetailActivity.java
+++ app/src/main/java/edu/tacoma/uw/udeal/ItemDisplaySellingDetailActivity.java
@@ -27,7 +27,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.ActionBar;
 
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import inbox.ChatActivity;
@@ -66,29 +68,7 @@ public class ItemDisplaySellingDetailActivity extends AppCompatActivity implemen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_display_detail);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Sending a message to the seller", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                SharedPreferences settings = getSharedPreferences((getString(R.string.LOGIN_PREFS)), Context.MODE_PRIVATE);
-                String current = settings.getString(getString(R.string.username), "");
-                // temporary, change to
-                UserInbox item = new UserInbox(current, mItemDisplay.getMyUsername(),
-                        mItemDisplay.getMyUsername(), mItemDisplay.getMyUsername());
-                Log.e("ItemDisplayDetailActivi", String.valueOf(item.getOtherUserName()));
-                Context context = view.getContext();
-                Intent intent = new Intent(context, ChatActivity.class);
-                intent.putExtra(ChatActivity.ARG_ITEM_ID, item);
-                context.startActivity(intent);
-            }
-        });
-
-
-
+        setContentView(R.layout.activity_item_display_selling_detail);
 
         if (savedInstanceState == null) {
             Bundle arguments = new Bundle();
@@ -117,8 +97,28 @@ public class ItemDisplaySellingDetailActivity extends AppCompatActivity implemen
             actionBar.setTitle("");
         }
 
+        Switch mSwitch = (Switch) findViewById((R.id.list_switch));
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb, boolean on) {
+                mItemDisplay.setMyListed(on);
+                if(on) {
+                    ((Switch) findViewById(R.id.list_switch)).setText("Your item is listed.");
+                } else {
+                    ((Switch) findViewById(R.id.list_switch)).setText("Your item is not listed.");
+                }
+            }
+        });
+
         if (mItemDisplay != null) {
             byte[] tmp = mItemDisplay.getMyBitmapArray();
+            ((Switch) findViewById(R.id.list_switch)).setChecked(mItemDisplay.getMyListed());
+            if(mItemDisplay.getMyListed())  {
+                mSwitch.setText("Your item is listed.");
+            } else {
+                mSwitch.setText("Your item is not listed.");
+            }
             ((ImageView) findViewById(R.id.item_image_id)).setImageBitmap(BitmapFactory.decodeByteArray(tmp, 0, tmp.length));
             ((TextView) findViewById(R.id.item_title)).setText(mItemDisplay.getMyTitle());
             ((TextView) findViewById(R.id.item_category)).setText("Category: " + mItemDisplay.getMyCategory());
@@ -139,7 +139,7 @@ public class ItemDisplaySellingDetailActivity extends AppCompatActivity implemen
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            navigateUpTo(new Intent(this, MainActivity.class));
+            navigateUpTo(new Intent(this, CartActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
