d7c6527ce2760e159659de754c45641390ca5fbf
mzloria <mikeeloria@gmail.com>
2020-05-22 16:11:07 -0700

    Function to write a review.

--- app/src/main/java/edu/tacoma/uw/udeal/WriteReview.java
+++ app/src/main/java/edu/tacoma/uw/udeal/WriteReview.java
@@ -19,8 +19,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -48,6 +53,26 @@ public class WriteReview extends Fragment {
     /** The JSON object for the arguments. */
     private JSONObject mArguments;
 
+    /** The dropdown menu. */
+    private Spinner dropdown;
+
+    /** The current category. */
+    private int currentCategory;
+
+    /** The category string. */
+    private String categoryString;
+
+    /** The review edit text. */
+    private EditText review;
+
+    /** The username edit text. */
+    private EditText username;
+
+    /** The rating bar. */
+    private RatingBar ratingBar;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -69,15 +94,130 @@ public class WriteReview extends Fragment {
                 getActivity().overridePendingTransition(0,0);
             }
         });
+
+        dropdown = view.findViewById(R.id.spinner);
+        final String[] items = new String[]{"Select your role", "Buyer", "Seller"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        currentCategory = 0;
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (currentCategory != position){
+                    categoryString = items[position];
+                } else {
+                    //top is correct way but doesn't work. second way works
+//                    ((TextView) view).setTextColor(getResources().getColor(R.color.colorPrimary));
+                    ((TextView) view).setTextColor(R.color.colorPrimary);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Nothing selected
+            }
+        });
+
         postReviewButton = (Button) view.findViewById(R.id.postreview);
+        review = (EditText) view.findViewById(R.id.review);
+        username = (EditText) view.findViewById(R.id.username);
+        ratingBar = (RatingBar) view.findViewById(R.id.stars);
 
         postReviewButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //String password = myPassword.getText().toString();
-                //onUpdatePassword(password);
+                String myReview = review.getText().toString();
+                String myUsername = username.getText().toString();
+                String myRole = dropdown.getSelectedItem().toString();
+                double myRating = ratingBar.getRating();
+                addReview(myReview, myUsername, myRole, myRating);
             }
         });
         return view;
+    }
+
+    private void addReview(String theReview, String theUsername, String theRole, double theRating) {
+        SharedPreferences settings = getActivity().getSharedPreferences(getString(R.string.LOGIN_PREFS),
+                Context.MODE_PRIVATE);
+        mArguments = new JSONObject();
+        try {
+            mArguments.put("reviewed", theUsername);
+            mArguments.put("reviewer", settings.getString(getString(R.string.username), "" ));
+            mArguments.put("rating", theRating);
+            mArguments.put("review", theReview);
+            mArguments.put("role", theRole);
+            new AddReviewAsyncTask().execute(getString(R.string.add_review));
+        } catch (JSONException e) {
+            Toast.makeText(getActivity().getApplicationContext(), "Error with JSON creation" +
+                    e.getMessage() , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * The async task to add an item.
+     *
+     * @author TCSS 450 Team 8
+     * @version 1.0
+     */
+    private class AddReviewAsyncTask extends AsyncTask<String, Void, String> {
+        /**
+         * Handles the post of the review's text fields to the database.
+         *
+         * @param urls The URL to establish the connection
+         * @return The result of the async task
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.setDoOutput(true);
+                    OutputStreamWriter wr =
+                            new OutputStreamWriter(urlConnection.getOutputStream());
+                    Log.i("myTag", mArguments.toString());
+                    wr.write(mArguments.toString());
+                    wr.flush();
+                    wr.close();
+                    InputStream content = urlConnection.getInputStream();
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+                } catch (Exception e) {
+                    response = "Unable to add the new item, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        /**
+         * If successful, the item is successfully posted.
+         *
+         * @param result The result of the async task
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject resultObject = new JSONObject(result);
+                if (resultObject.getBoolean("success") == true) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Successfully posted the review", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Missing information", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
