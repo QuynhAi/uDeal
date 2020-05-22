c45ecabea45410146779084deed78d6c83c5b2d2
mzloria <mikeeloria@gmail.com>
2020-05-22 15:32:03 -0700

    Write a review layout complete

--- app/src/main/java/edu/tacoma/uw/udeal/WriteReview.java
+++ app/src/main/java/edu/tacoma/uw/udeal/WriteReview.java
@@ -42,11 +42,8 @@ import java.net.URL;
  */
 public class WriteReview extends Fragment {
 
-    /** The edit text for the password. */
-    private EditText myPassword;
-
     /** The update password button. */
-    private Button updateBtn;
+    private Button postReviewButton;
 
     /** The JSON object for the arguments. */
     private JSONObject mArguments;
@@ -72,97 +69,15 @@ public class WriteReview extends Fragment {
                 getActivity().overridePendingTransition(0,0);
             }
         });
-        myPassword = (EditText) view.findViewById(R.id.newpw);
-        updateBtn = (Button) view.findViewById(R.id.updatepw);
+        postReviewButton = (Button) view.findViewById(R.id.postreview);
 
-        updateBtn.setOnClickListener(new View.OnClickListener(){
+        postReviewButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                String password = myPassword.getText().toString();
-                onUpdatePassword(password);
+                //String password = myPassword.getText().toString();
+                //onUpdatePassword(password);
             }
         });
         return view;
     }
 
-    private void onUpdatePassword(String password) {
-        StringBuilder url = new StringBuilder(getString(R.string.change_password));
-        SharedPreferences settings = getActivity().getSharedPreferences(getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
-        String username = settings.getString(getString(R.string.username), "");
-        mArguments = new JSONObject();
-        try {
-            mArguments.put("username", username);
-            mArguments.put("password", password);
-            new UpdatePasswordAsyncTask().execute(url.toString());
-        } catch (JSONException e) {
-            Toast.makeText(getActivity(), "Error with JSON creation: " + e.getMessage() , Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    /**
-     * Async task that adds the user to the database.
-     *
-     * @author TCSS 450 Team 8
-     * @version 1.0
-     */
-    private class UpdatePasswordAsyncTask extends AsyncTask<String, Void, String> {
-        /**
-         * Performs the async task to register the user in the database.
-         *
-         * @param urls The url endpoint for the registration
-         * @return The response from the async task
-         */
-        @Override
-        protected String doInBackground(String... urls) {
-            String response = "";
-            HttpURLConnection urlConnection = null;
-            for (String url : urls) {
-                try {
-                    URL urlObject = new URL(url);
-                    urlConnection = (HttpURLConnection) urlObject.openConnection();
-                    urlConnection.setRequestMethod("PUT");
-                    urlConnection.setRequestProperty("Content-Type", "application/json");
-                    urlConnection.setDoOutput(true);
-                    OutputStreamWriter wr =
-                            new OutputStreamWriter(urlConnection.getOutputStream());
-                    Log.i("myTag", mArguments.toString());
-                    wr.write(mArguments.toString());
-                    wr.flush();
-                    wr.close();
-                    InputStream content = urlConnection.getInputStream();
-                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
-                    String s = "";
-                    while ((s = buffer.readLine()) != null) {
-                        response += s;
-                    }
-                } catch (Exception e) {
-                    response = "Unable to update the password, Reason: "
-                            + e.getMessage();
-                } finally {
-                    if (urlConnection != null)
-                        urlConnection.disconnect();
-                }
-            }
-            return response;
-        }
-
-        /**
-         * If password update is successful, a toast is displayed saying it was successful. If the
-         * update is unsuccessful, the toast is displayed saying it was unsuccessful.
-         *
-         * @param result The result from the async task
-         */
-        @Override
-        protected void onPostExecute(String result){
-            try{
-                JSONObject resultObject = new JSONObject(result);
-                if(resultObject.getBoolean("success") == true){
-                    Toast.makeText(getActivity(), "Password updated successfully.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getActivity(), "Password update was unsuccessful.", Toast.LENGTH_SHORT).show();
-                }
-            }catch(JSONException e){
-                Toast.makeText(getActivity(), e.getMessage() , Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
